46708666d5add161464b51502525d01e4f83cba9
sbeltier <sarahnbeltier@gmail.com>
2021-10-21 22:20:17 -0700

    update

--- public/db.js
+++ public/db.js
@@ -27,18 +27,13 @@ request.onerror = function (e) {
     console.log(`Error:`+ e.target.errorCode);
   };
 
-  function checkDatabase() {
-    console.log('check db invoked');
-  
+  function checkDatabase() {  
     let transaction = db.transaction(['TransactionStore'], 'readwrite');
     const store = transaction.objectStore('TransactionStore');
   
-    // Get all records from store and set to a variable
     const getAll = store.getAll();
   
-    // If the request was successful
     getAll.onsuccess = function () {
-      // If there are items in the store, we need to bulk add them when we are back online
       if (getAll.result.length > 0) {
         fetch('/api/transaction/bulk', {
           method: 'POST',
@@ -50,17 +45,10 @@ request.onerror = function (e) {
         })
           .then((response) => response.json())
           .then((res) => {
-            // If our returned response is not empty
             if (res.length !== 0) {
-              // Open another transaction to BudgetStore with the ability to read and write
               transaction = db.transaction(['TransactionStore'], 'readwrite');
-  
-              // Assign the current store to a variable
-              const currentStore = transaction.objectStore('TransactionStore');
-  
-              // Clear existing entries because our bulk add was successful
-              currentStore.clear();
-              console.log('Clearing store 🧹');
+                const currentStore = transaction.objectStore('TransactionStore');
+                currentStore.clear();
             }
           });
       }
